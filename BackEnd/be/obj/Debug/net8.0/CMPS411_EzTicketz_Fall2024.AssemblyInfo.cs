207ca7e2fb78c13193d624393514cff79a2fd08d
Jake Saurage <jake.saurage03@gmail.com>
2024-10-22 16:55:46 -0500

    Added GitIgnore

--- BackEnd/be/obj/Debug/net8.0/CMPS411_EzTicketz_Fall2024.AssemblyInfo.cs
+++ BackEnd/be/obj/Debug/net8.0/CMPS411_EzTicketz_Fall2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMPS411_EzTicketz_Fall2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a19abde9442069caa019c1a37aeef2e80970d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca12a659fe94e553bdd8287a7487ab76a628f794")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMPS411_EzTicketz_Fall2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMPS411_EzTicketz_Fall2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
